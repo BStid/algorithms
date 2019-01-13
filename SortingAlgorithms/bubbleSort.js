f6cc1eb51c98d4e3219adbbcf933301d0b4a4587
Benjamin Stidham <benjamindstidham@gmail.com>
2019-01-12 19:07:13 -0600

    added notes to bubbleSort

--- SortingAlgorithms/bubbleSort.js
+++ SortingAlgorithms/bubbleSort.js
@@ -1,9 +1,13 @@
 const bubbleSort = array =>{
+    //loop through the original array...
     for(let i = 0; i < array.length - 1; i++){
         let change = false;
+        //loop through the whole array again for each element...
         for (let j = 0; j < array.length - (i + 1); j++){
+            //if the current index is greater than the next index...
             if(array[j] > array[j + 1]){
                 change = true;
+                //switch the places of the indices
                 [array[j], array[j + 1]] = [array[j + 1], array[j]];
             }
         }
