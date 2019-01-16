3c886cea3b41398157b34e4dde8d9712755d86c7
Benjamin Stidham <benjamindstidham@gmail.com>
2019-01-15 23:45:54 -0600

    added notes to heapSort

--- SortingAlgorithms/heapSort.js
+++ SortingAlgorithms/heapSort.js
@@ -7,31 +7,42 @@ function maxHeapify(array, i){
     let left = 2 * i + 1;
     let right = 2 * i + 2;
     let max = i;
+    
 
+    //if the 'left' number is less than 'length'
+    //and the element specified as left is greater than the last index..
     if(left < length && array[left] > array[max]){
+        //then max now equals the index specified for the left side
         max  = left;
     }
 
     if(right < length && array[right] > array[max]){
         max = right;
     }
+    //if max does not equal the original 'i'...
     if(max != i){
+        //swap the last element and the current greater number..
         swap(array, i, max);
+        //run the function again
         maxHeapify(array, max);
     }
 }
 
 function swap(array, i, j){
+    //temp variable holding the current value 
     let temp = array[i];
 
+    //this value now becomes the the value of index 'j'
     array[i] = array[j];
+    //index 'j' now becomes the value of 'temp'
     array[j] = temp;
 }
 
 function heapSort(array){
     length = array.length;
 
-    for(let i = Math.floor(length / 2); i >=0; i -= 1){
+
+    for(let i = Math.floor(length / 2); i >=0; i--){
         maxHeapify(array, i);
     }
 
